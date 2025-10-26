3f98252fd7149f787c53aef438847f1cc6ad4f19
rajin-siam <mdrajinmashrursiam@gmail.com>
2025-10-27 00:44:36 +0600

    fix(auth): complete authentication system fixes

---
+++ HealthLink.Core/Models/Auth/RevokeTokenRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthLink.Core.Models.Auth
+{
+    internal class RevokeTokenRequest
+    {
+    }
+}
